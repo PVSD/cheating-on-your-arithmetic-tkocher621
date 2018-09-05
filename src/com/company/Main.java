524e024b32b4dee6031bb33202140aa20eaa3c41
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2018-09-05 17:00:38 -0400

    Update Main.java
    hey wanna hear a funny joke cause my heart just stopped

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Tester a = new Tester();
+
+        System.out.println("79 + 3 * (4+ 82 - 68) - 7 + 19 = " + a.Problem1());
+        System.out.println("(179 + 21 + 10) /7 + 181) = " + a.Problem2());
+        System.out.println("10359 * 56 * 11 + 2246 = " + a.Problem3() );
+
     }
 }
