37011c067204199da08b2c272d4cdd5347739da7
Todd <tk304@pvsd.org>
2018-09-05 16:42:49 -0400

    hey wanna hear a funny joke cuz my heart just stopped

---
+++ src/com/company/Tester.java
@@ -0,0 +1,19 @@
+package com.company;
+
+class Tester {
+
+    int Problem1()
+    {
+        return 79+3*(4+82-68)-7+19;
+    }
+
+    int Problem2()
+    {
+        return (179+21+10)/7+181;
+    }
+
+    int Problem3()
+    {
+        return 10389 * 56 * 11 + 2246;
+    }
+}
